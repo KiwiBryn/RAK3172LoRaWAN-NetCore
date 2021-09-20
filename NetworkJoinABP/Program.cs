e1eb44abedd81a241c0aef1cd5b472de69f3c678
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-09-20 19:29:13 +1200

    First working version of NastyABP connect

--- NetworkJoinABP/Program.cs
+++ NetworkJoinABP/Program.cs
@@ -17,12 +17,150 @@
 namespace devMobile.IoT.NetCore.RAK3172.NetworkJoinABP
 {
 	using System;
+	using System.Diagnostics;
+	using System.IO.Ports;
+	using System.Threading;
 
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		private const string SerialPortId = "/dev/ttyS0";
+		private const string DevAddress = "...";
+		private const string NwksKey = "...";
+		private const string AppsKey = "...";
+		private const byte MessagePort = 1;
+		private const string Payload = "A0EEE456D02AFF4AB8BAFD58101D2A2A"; // Hello LoRaWAN
+
+		public static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			string response;
+
+			Debug.WriteLine("devMobile.IoT.NetCore.Rak3172.NetworkJoinOTAA starting");
+
+			Debug.WriteLine(String.Join(",", SerialPort.GetPortNames()));
+
+			try
+			{
+				using (SerialPort serialPort = new SerialPort(SerialPortId))
+				{
+					// set parameters
+					serialPort.BaudRate = 9600;
+					serialPort.DataBits = 8;
+					serialPort.Parity = Parity.None;
+					serialPort.StopBits = StopBits.One;
+					serialPort.Handshake = Handshake.None;
+
+					serialPort.ReadTimeout = 5000;
+
+					serialPort.NewLine = "\r\n";
+
+					serialPort.Open();
+
+					// clear out the RX buffer
+					response = serialPort.ReadExisting();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+					Thread.Sleep(500);
+
+					// Set the Working mode to LoRaWAN
+					Console.WriteLine("Set Work mode");
+					serialPort.WriteLine("AT+NWM=1");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the Region to AS923
+					Console.WriteLine("Set Region");
+					serialPort.WriteLine("AT+BAND=8-1");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the JoinMode
+					Console.WriteLine("Set Join mode");
+					serialPort.WriteLine("AT+NJM=0");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the device address
+					Console.WriteLine("Set Device Address");
+					serialPort.WriteLine($"AT+DEVADDR={DevAddress}");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the network session key
+					Console.WriteLine("Set Network Session Key");
+					serialPort.WriteLine($"AT+NWKSKEY={NwksKey}");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the application session key
+					Console.WriteLine("Set application Session Key");
+					serialPort.WriteLine($"AT+APPSKEY={AppsKey}");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Set the Confirm flag
+					Console.WriteLine("Set Confirm off");
+					serialPort.WriteLine("AT+CFM=0");
+					// Read the blank line
+					response = serialPort.ReadLine();
+					// Read the response
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					// Join the network
+					Console.WriteLine("Start Join");
+					serialPort.WriteLine("AT+JOIN=1:0:10:2");
+
+					// Read the blank line
+					response = serialPort.ReadLine();
+
+					// Read the Result
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					Thread.Sleep(10000);
+
+					// Read the +EVT:JOINED
+					response = serialPort.ReadLine();
+					Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+					while (true)
+					{
+						Console.WriteLine("Sending");
+						serialPort.WriteLine($"AT+SEND={MessagePort}:{Payload}");
+
+						// Read the blank line
+						response = serialPort.ReadLine();
+
+						// Read the result
+						Console.WriteLine("Send result");
+						response = serialPort.ReadLine();
+						Debug.WriteLine($"RX :{response.Trim()} bytes:{response.Length}");
+
+						Thread.Sleep(300000);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
 		}
 	}
 }
